b607eda52c67b13562721db0fa6e4c9702abae96
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-11-27 20:29:19 +0100

    feat: Model validation

--- models/location.py
+++ models/location.py
@@ -1,10 +1,22 @@
 # Python
 from typing import Optional
 # Pydantic
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Location(BaseModel):
-    city: str
-    state: str
-    country: str
+    city: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    state: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    country: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
--- models/person.py
+++ models/person.py
@@ -1,15 +1,40 @@
 # Python
 from typing import Optional
+from enum import Enum
 # Pydantic
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+
+class HairColor(Enum):
+    white = "white"
+    brown = "brown"
+    black = "black"
+    blonde = "blonde"
+    red = "red"
 
 
 class Person(BaseModel):
-    first_name: str
-    last_name: str
-    hair_color: Optional[str] = None
-    biography: Optional[str] = None
-    is_married: Optional[bool] = None
-    age: int
-    height: float
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    hair_color: Optional[HairColor] = Field(default=None)
+    biography: Optional[str] = Field(default=None)
+    is_married: Optional[bool] = Field(default=None)
+    age: int = Field(
+        ...,
+        gt=0,
+        le=115
+    )
+    height: float = Field(
+        ...,
+        gt=0,
+        le=300
+    )
     weight: Optional[float] = None
